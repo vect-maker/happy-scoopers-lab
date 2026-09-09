9505a0e89b1b82bb2a45e3de4dbc41bd52263eea
vect-maker <josedanielmirandaperez410@gmail.com>
2026-09-09 10:43:13 -0600

    feat: staging producto

---
+++ transform/models/staging/stg_oltp__product_departments.sql
@@ -0,0 +1,6 @@
+with source as ( select * from {{ source(‘raw', ‘product_departments') }} )
+select
+    department_id,
+    name as department_name,
+    description as department_description
+from source
---
+++ transform/models/staging/stg_oltp__products.sql
@@ -0,0 +1,6 @@
+with source as ( select * from {{ source('r , ‘products') }} )
+select
+    product_id, product_name, product_code, product_description,
+    subcategory id, unit_of_measure id, unit_price,
+    case when discontinued then 'Si' else ‘No’ end as is discontinued
+from source
