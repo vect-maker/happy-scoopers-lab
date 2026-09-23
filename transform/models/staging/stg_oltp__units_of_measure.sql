6101804350e471fe33a30c9e8a06a7decab4cf6f
vect-maker <josedanielmirandaperez410@gmail.com>
2026-09-23 10:48:52 -0600

    feat: a

--- transform/models/staging/stg_oltp__units_of_measure.sql
+++ transform/models/staging/stg_oltp__units_of_measure.sql
@@ -1,4 +1,10 @@
-with source as ( select * from {{ source('raw', 'units_of_measure') }} )
+with source as (
+    select * from {{ source('raw', 'units_of_measure') }}
+)
 
-SELECT  unit_of_measure_id, unit_measure_code, name, modified_date
-	FROM source
+select
+    unit_of_measure_id,
+    unit_measure_code as unit_of_measure_code,
+    name as unit_of_measure_name,
+    modified_date
+from source
