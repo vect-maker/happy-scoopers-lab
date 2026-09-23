f80654130a0e37ca85ac9213a86c002a045a3936
vect-maker <josedanielmirandaperez410@gmail.com>
2026-09-23 10:00:14 -0600

    feat: add some tables

--- el/pipeline.py
+++ el/pipeline.py
@@ -47,10 +47,10 @@ DESTINATIONS = {
 # reciben datos, viajarán solos)
 TABLES = [
     # Esta lista crece laboratorio a laboratorio.
-    # Lab 2: "payment_types"
-    # Lab 3: + "countries", "provinces", "cities", "addresses"
-    # Lab 4: + "products", "product_subcategories",
-    #          "product_categories", "product_departments", "units_of_measure"
+     "payment_types",
+     "countries", "provinces", "cities", "addresses",
+     "products", "product_subcategories",
+     "product_categories", "product_departments", "units_of_measure"
     # Lab 5: + "customers", "employees", "promotions"
     # Lab 6: + "orders", "order_lines", "package_types"
 ]
